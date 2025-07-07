e30297a85d0d2b13905d6c17d504b51c92b3c16d
Aleksandar Atanasov <aleks930819@gmail.com>
2025-07-07 21:33:36 +0300

    feat: enhance accessibility by adding tabindex and aria-labels to components

--- src/app/core/how-it-works/how-it-works.component.ts
+++ src/app/core/how-it-works/how-it-works.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { HowItWorksStepComponent } from "./how-it-works-step/how-it-works-step.component";
+import { HowItWorksStepComponent } from './how-it-works-step/how-it-works-step.component';
 
 @Component({
   selector: 'app-how-it-works',
   standalone: true,
   imports: [HowItWorksStepComponent],
   templateUrl: './how-it-works.component.html',
-  styleUrl: './how-it-works.component.css'
+  styleUrl: './how-it-works.component.css',
 })
-export class HowItWorksComponent {
-
-}
+export class HowItWorksComponent {}
