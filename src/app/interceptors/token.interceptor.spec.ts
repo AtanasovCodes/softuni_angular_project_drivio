4462e8538e56876fde00e8ce597c70ff6037a708
Aleksandar Atanasov <aleks930819@gmail.com>
2025-07-17 17:39:21 +0300

    test: Add unit tests for token interceptor functionality

---
+++ src/app/interceptors/token.interceptor.spec.ts
@@ -0,0 +1,52 @@
+import { LOCAL_STORAGE_KEYS } from 'constants/local-storage';
+
+import { HttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+
+import { tokenInterceptor } from './token.interceptor';
+
+describe('TokenInterceptor', () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(withInterceptors([tokenInterceptor])),
+        provideHttpClientTesting(),
+      ],
+    });
+
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  it('should add Authorization header if token exists in localStorage', () => {
+    localStorage.setItem(LOCAL_STORAGE_KEYS.TOKEN_KEY, 'mock-token');
+
+    httpClient.get('/test-endpoint').subscribe();
+
+    const req = httpTestingController.expectOne('/test-endpoint');
+    expect(req.request.headers.has('Authorization')).toBeTrue();
+    expect(req.request.headers.get('Authorization')).toBe('Bearer mock-token');
+
+    req.flush({});
+  });
+
+  it('should not add Authorization header if token does not exist', () => {
+    localStorage.removeItem(LOCAL_STORAGE_KEYS.TOKEN_KEY);
+
+    httpClient.get('/test-endpoint').subscribe();
+
+    const req = httpTestingController.expectOne('/test-endpoint');
+    expect(req.request.headers.has('Authorization')).toBeFalse();
+
+    req.flush({});
+  });
+});
